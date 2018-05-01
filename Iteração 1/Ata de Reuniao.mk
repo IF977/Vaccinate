c223ccedbced7180f38ca061f9f6c47a14a0a772
Ricardo Ferreira dos Santos <rfsj@cin.ufpe.br>
2018-04-30 22:08:24 -0300

    Ata de reunião e Postmortem

---
+++ Iteração 1/Ata de Reuniao.mk
@@ -0,0 +1,34 @@
+## Ata de Reunião - Iteração 1
+
+| Data  | Local |
+| -  | -  |
+| 30/04/2018 | Discord |
+
+### Objetivos
+
+- Criar processo de verificação/autenticação de requisição
+- Criação de CRUD's
+
+### Atividades realizadas
+
+- Construção do modelo Entidade-Relacionamento
+- Construção do modelo Lógico
+- Início do processo de verificação/autenticação de requisição
+- Criação de Usuário
+
+### Divisão de papéis
+
+| Participante  | Função |
+| -  | - |
+| Matheus Gurjão | Desenvolvedor de Testes |
+| Ricardo Ferreira | Product Owner e Desenvolvedor Back-end |
+| Ruan Vieira | Desenvolvedor Front-end e de Testes |
+| Vitor Lima | Gerente de Projeto e Desenvolvedora Full-stack |
+### Divisão de atividades
+
+| Atividade  | Membro(s) |
+| -  | - |
+| Construção do modelo Entidade-Relacionamento | Time |
+| Construção do modelo Lógico | Ruan Vieira |
+| Início do processo de verificação/autenticação de requisição | Vitor Lima |
+| Criação de Usuário | Ricardo Ferreira|
---
+++ Iteração 1/Postmortem.mk
@@ -0,0 +1,32 @@
+# Postmortem - Iteração 1
+| Data  | Local |
+| -  | -  |
+| 30/04/2018 | Discord |
+
+### Período
+16/04/18 — 30/04/18
+
+### O que estava planejado?
+| Atividade  | Membro(s) |
+| -  | - |
+| Construção do modelo Entidade-Relacionamento | Time |
+| Construção do modelo Lógico | Ruan Vieira |
+| Início do processo de verificação/autenticação de requisição | Vitor Lima |
+| Criação de Usuário | Ricardo Ferreira |
+
+### O que foi, e o que não foi feito?
+Todas as atividades listadas acima, porém o processo de verificação/autenticação de requisição não conseguiu ser concluído. 
+
+### O que está planejado para a próxima iteração?
+| Atividade  | Membro(s) |
+| -  | - |
+| Continuação do processo de verificação/autenticação de requisição  | Vitor Lima |
+| Identidade visual da aplicação | Time |
+| configuração de rotas | Gabriel D'Luca |
+| Front-end da página inicial | Vitor Lima, Ruan Vieira e Ricardo Ferreira |
+| Deploy inicial | Time |
+
+### Lições aprendidas (Postmortem / Rationale)
+* Realizar as tarefas com antecedência;
+* Ter mais iniciativa por parte da equipe;
+* Marcar mais reuniões para alinhamento do projeto.
